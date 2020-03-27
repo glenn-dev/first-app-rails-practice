dbb7d26ad54a9383241c7c8d233cf3176189588d
Glenn <glenn.marcano90@gmail.com>
2020-03-27 13:48:08 -0300

    Navbar added, routes fixed. Final version.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/one'
-  get 'pages/two'
-  get 'pages/three'
+  get 'pages/two', to: 'pages#two', as: 'two'
+  get 'pages/three', to: 'pages#three', as: 'three'
 
   root 'pages#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
